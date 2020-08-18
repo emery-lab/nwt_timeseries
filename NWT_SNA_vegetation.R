146d022eda27a2d59c353a3c9f196f3d4d9ae3ed
MeaganOldfather <meagan.f.oldfather@gmail.com>
2020-08-18 11:03:43 -0600

    Created a psi matrix that has all correct combinations of sensor nodes

--- NWT_SNA_vegetation.R
+++ NWT_SNA_vegetation.R
@@ -8,46 +8,58 @@ library(cowplot)
 library(sf)
 library(ecodist)
 
-# plot time-series psi metric
-psi <- read.csv("data/08.12.20.sennetdissimilarity.csv")
-head(psi)
-colnames(psi) <- c("s1", "s2", "psi")
-length(unique(psi$s1)) # 16
-length(unique(psi$s2)) # 16
+# read in time-series psi metric
+psi.df <- read.csv("data/08.12.20.sennetdissimilarity.csv")
+head(psi.df)
 
-#ggplot(psi, aes(x = s1, y = psi))+
-#  geom_point()+
-#  theme_classic()
+# remove 'old psi column
+psi.df <- psi.df [, -4]
 
+# update column names 
+colnames(psi.df) <- c("s1", "s2", "psi")
 
-psi.matrix <- spread(psi, s1, psi)
-psi.matrix[is.na(psi.matrix)] <- 0
-psi.matrix$s2 # contains a plots 18, but no plot 1
-dim(psi.matrix)
+#look at sensor node numbers
+unique(psi.df$s1)
+length(unique(psi.df$s1)) # 14
+unique(psi.df$s2)
+length(unique(psi.df$s2)) # 14
+psi.df
 
-# remove s2 column
-psi.matrix <- psi.matrix[,-1]
-dim(psi.matrix)
+# duplicate all sensor nodes so that all combinations exist
+psi.dup <- psi.df %>% 
+  mutate(temp = s2, s2 = s1, s1 = temp) %>% 
+  select(-temp) %>%
+  rbind(psi.df) %>% 
+  rbind(data.frame(s1 = c(6:17, 19:21), s2 = c(6:17, 19:21), psi = 0))
+psi.dup
 
-# remove plot 18 to match the veg data
-psi.matrix <- psi.matrix[,-13] # remove column
-psi.matrix <- psi.matrix[rownames(psi.matrix) != "13",] # remove row
-plots <- c(6:17, 19:21) # remake plots to not contain 18
-
-psi.matrix <- as.matrix(psi.matrix)
-row.names(psi.matrix) <- plots
+# turn psi df into a matrix
+psi.matrix <- 
+  psi.dup %>% 
+  arrange(s1,s2) %>% 
+  pivot_wider(id_cols = s1, names_from = s2, values_from = psi) %>% 
+  select(-s1)
 psi.matrix
 dim(psi.matrix)
 
+psi.matrix <- as.matrix(psi.matrix)
+row.names(psi.matrix) <- colnames(psi.matrix)
+
+
+##### still need to rearrange the veg matrix to now match the psi.matrix
+
 # plot plant community data
-veg <- read.csv("06.28.20_sennet_plantcommunity_raw.csv") 
+veg <- read.csv("raw_data/06.28.20_sennet_plantcommunity_raw.csv") 
 head(veg)
-unique(veg$sensor_node) # contains a plot 1, but not 18
-unique(veg$date) # multiple years of data, subset to 2019
+unique(veg$sensor_node) 
+length(unique(veg$sensor_node)) # 17 nodes here -- > need to pair down to 14 from which psi is calculated
+unique(veg$date) # multiple years of data
+
+# subset
 veg <- subset(veg, year == 2019 & abundance!= 0.5 & multi.hit_or_top.hit == "top-hit-of-multihit-protocol")
 head(veg)
 
-# subset to columns of interet  (plot, species, abundance)
+# subset to columns of interest (plot, species, abundance)
 veg <- veg[,c("sensor_node", "abundance", "species")]
 
 # remove plot 1 
@@ -65,8 +77,7 @@ non_veg <- c("Rock", "Unknown_seedling", "Unknown_graminoid", "Soil", "Soil ","L
 
 veg.matrix <- veg.matrix[, !colnames(veg.matrix) %in% non_veg]
 
-
-# calculate dissimilarity 
+# calculate Bray-Curtis dissimilarity 
 veg.dissimilarity <- vegdist(x = veg.matrix, method = "bray", upper = T, diag = T)
 veg.dissimilarity.matrix <- as.matrix(veg.dissimilarity)
 dim(veg.dissimilarity.matrix)
@@ -132,8 +143,6 @@ plot_grid(veg.plot, psi.plot)
 head(combined)
 
 #write.csv(combined, "bray_psi_metrics.csv", row.names = F)
-
-
 
 #### Make ordination of community data ####
 head(veg.matrix)
