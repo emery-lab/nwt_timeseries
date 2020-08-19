74fe3811b63290b2143093b18eb4d3de990ed23e
MeaganOldfather <meagan.f.oldfather@gmail.com>
2020-08-18 20:13:28 -0600

    Reran analysis lookoing at how psi infleunces vegetation dissimilarity

--- NWT_SNA_vegetation.R
+++ NWT_SNA_vegetation.R
@@ -46,27 +46,24 @@ dim(psi.matrix)
 psi.matrix <- as.matrix(psi.matrix)
 row.names(psi.matrix) <- colnames(psi.matrix)
 
-
 ##### still need to rearrange the veg matrix to now match the psi.matrix
 
 # plot plant community data
 veg <- read.csv("raw_data/06.28.20_sennet_plantcommunity_raw.csv") 
 head(veg)
-unique(veg$sensor_node) 
+unique(veg$sensor_node) %in% colnames(psi.matrix)
 length(unique(veg$sensor_node))# 17 nodes here -- > need to pair down to 14 from which psi is calculated
-unique(veg$sensor_node)
-unique(veg$date) # multiple years of data
 
-# subset
+# subset veg to plots with psi calculated
+veg <- subset(veg,veg$sensor_node %in% colnames(psi.matrix))
+
+# subset data to 2019 etc.
 veg <- subset(veg, year == 2019 & abundance!= 0.5 & multi.hit_or_top.hit == "top-hit-of-multihit-protocol")
 head(veg)
 
 # subset to columns of interest (plot, species, abundance)
 veg <- veg[,c("sensor_node", "abundance", "species")]
 
-# remove plot 1 
-veg <- subset(veg, sensor_node != 1)
-unique(veg$sensor_node) %in% plots # matches plots in psi metric
 
 # make df a community matrix
 veg.matrix <- sample2matrix(veg)
@@ -76,7 +73,6 @@ veg.matrix
 # remove 'non-veg' rows
 colnames(veg.matrix) #Rock, Unknown_seedling, Unknown_graminoid, Soil, Litter, Lichen, Moss
 non_veg <- c("Rock", "Unknown_seedling", "Unknown_graminoid", "Soil", "Soil ","Litter", "Lichen", "Moss")
-
 veg.matrix <- veg.matrix[, !colnames(veg.matrix) %in% non_veg]
 
 # calculate Bray-Curtis dissimilarity 
@@ -88,12 +84,15 @@ veg.dissimilarity.matrix
 # 2 matrices
 psi.matrix; dim(psi.matrix)
 veg.dissimilarity.matrix; dim(veg.dissimilarity.matrix)
+image(psi.matrix)
+image(veg.dissimilarity.matrix)
+
 
 vegan::mantel(ydis = psi.matrix, xdis = veg.dissimilarity.matrix)
-# almost 40% correlated! sig = 0.026
+#40% correlated! sig = 0.004
 
 # bring in physical distance
-physical.distance <- read.csv("06.29.2020.sensorlocations.csv")
+physical.distance <- read.csv("data/06.29.2020.sensorlocations.csv")
 physical.distance
 
 # remove plot 1 
@@ -102,7 +101,7 @@ physical.distance
 physical.distance <- sf::st_as_sf(physical.distance, coords = c("long", "lat"), crs = 4326)
 physical.distance
 
-distance <- st_distance(physical.distance) # no longer working due to package issues
+distance <- st_distance(physical.distance)
 distance
 dim(distance)
 
@@ -111,13 +110,17 @@ distance; dim(distance)
 psi.matrix; dim(psi.matrix)
 veg.dissimilarity.matrix; dim(veg.dissimilarity.matrix)
 
-# Fromm MMRRTutorial.R
-Xmats <- list(ecology=psi.matrix)
-str(Xmats)
-MMRR(veg.dissimilarity.matrix,Xmats, nperm = 10000)
+ggplot(as.data.frame(psi.matrix))+
+  geom_raster()
 
-##### Turn them into data frames ####
-veg.dissimilarity.df <- gather(veg.dissimilarity, "s2", "dis")
+# Function from MMRRTutorial.R
+Xmats <- list(ecology=psi.matrix, physical = distance)
+str(Xmats)
+MMRR(veg.dissimilarity.matrix,Xmats, nperm = 10000) # R.sq ~ 16%, only psi signficant
+
+##### Turn them into data frames - doesn't currently work ###
+
+veg.dissimilarity.df <- spread(veg.dissimilarity, "s2", "dis")
 veg.dissimilarity.df$s1 <- colnames(veg.dissimilarity) 
 veg.dissimilarity.df <- veg.dissimilarity.df[, c("s1", "s2", "dis")]
 veg.dissimilarity.df
